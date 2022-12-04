a409cb3c9a088c62a062a45292153938a0cb9180
Mehar Bilal <meharbilal24525@gmail.com>
2022-12-04 17:01:12 +0500

    Remove Unnecessary Usings

--- Tutoronic/Controllers/SubCategoriesController.cs
+++ Tutoronic/Controllers/SubCategoriesController.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using Tutoronic.Models;
 using Tutoronic.Request;
 using Tutoronic.Response;
 using Tutoronic.Services.Interface;
